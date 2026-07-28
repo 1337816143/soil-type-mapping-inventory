eed97fcc4ef769a914096e2b8fc33c54c1311398
1337816143 <1337816143@qq.com>
2026-07-28 10:34:21 +0800

    test: add one-command v1.0.6 validation suite

---
+++ scripts/test-v1.0.6.js
@@ -0,0 +1,8 @@
+'use strict';
+
+require('./validate-upload-token.js');
+require('./validate-reply-workflow.js');
+require('./validate-upload-success-notice.js');
+require('./validate-project.js');
+
+console.log('v1.0.6 complete validation passed');
