cd0763c8836e436cd29b95a6d9fade9c5b85901f
1337816143 <1337816143@qq.com>
2026-08-12 16:25:07 +0800

    release: display v1.0.14

--- app-release-ui.js
+++ app-release-ui.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  var VERSION = 'v1.0.13';
+  var VERSION = 'v1.0.14';
   window.SOIL_APP_VERSION = VERSION;
   document.documentElement.setAttribute('data-app-version', VERSION);
 
