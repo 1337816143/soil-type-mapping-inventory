3490f7dca62174a035d81a7e96952f1e64aa077c
1337816143 <1337816143@qq.com>
2026-08-13 11:48:51 +0800

    chore: bump release UI to v1.0.17

--- app-release-ui.js
+++ app-release-ui.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  var VERSION = 'v1.0.16';
+  var VERSION = 'v1.0.17';
   window.SOIL_APP_VERSION = VERSION;
   document.documentElement.setAttribute('data-app-version', VERSION);
 
