826c9bbe044e0817d7f2731df670c2d201385515
1337816143 <1337816143@qq.com>
2026-08-19 21:30:50 +0800

    chore: sync release UI to v1.1.5

--- app-release-ui.js
+++ app-release-ui.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  var VERSION = 'v1.1.4';
+  var VERSION = 'v1.1.5';
   window.SOIL_APP_VERSION = VERSION;
   document.documentElement.setAttribute('data-app-version', VERSION);
 
